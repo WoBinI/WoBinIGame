27961ef72d91adec7fe4b86edfac4ad919096192
feitzi <mathias@feitzinger.org>
2016-12-27 12:45:28 +0100

    angular doc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
+import { HomeComponent } from "./home/home.component";
 import { StreetViewComponent } from './StreetView/StreetView.component';
 
-import {NgModule} from '@angular/core'
-import {RouterModule} from "@angular/router";
-import {rootRouterConfig} from "./app.routes";
-import {AppComponent} from "./app.component";
-import {FormsModule} from "@angular/forms";
-import {BrowserModule} from "@angular/platform-browser";
-import {HttpModule} from "@angular/http";
-import {HomeComponent} from './home/home.component';
-import {LocationStrategy, HashLocationStrategy} from '@angular/common';
+import { NgModule } from '@angular/core'
+import { RouterModule } from "@angular/router";
+import { rootRouterConfig } from "./app.routes";
+import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpModule } from "@angular/http";
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { AgmCoreModule } from 'angular2-google-maps/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent, StreetViewComponent, HomeComponent],
-  imports     : [BrowserModule, FormsModule, HttpModule,
-   RouterModule.forRoot(rootRouterConfig), AgmCoreModule.forRoot({
-      apiKey: '511a94b8-8bdf-447b-9959-42783ddbd02d'
-    })],
-  providers   : [{provide: LocationStrategy, useClass: HashLocationStrategy}],
-  bootstrap   : [AppComponent]
+  imports: [BrowserModule, FormsModule, HttpModule,
+    RouterModule.forRoot(rootRouterConfig), AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyClomCuWY6L5pul5w6xnHrIUbOGBzRbnEU'
+    }), NgbModule.forRoot()
+  ],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 
