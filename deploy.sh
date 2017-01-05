8e0c2eb45c2a8fd605de2705462a276565c38e3f
feitzi <mathias@feitzinger.org>
2017-01-05 15:15:08 +0100

    changed CI target branch

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e # Exit with nonzero exit code if anything fails
 
 SOURCE_BRANCH="master"
-TARGET_BRANCH="ci-deployment"
+TARGET_BRANCH="gh-pages"
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
@@ -15,8 +15,8 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Clone the existing ci-deployment for this repo into gitCiOuput/
-# Create a new empty branch if ci-deployment doesn't exist yet (should only happen on first deply)
+# Clone the existing gh-pages for this repo into gitCiOuput/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
 git clone $REPO gitCiOuput
 cd gitCiOuput
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
