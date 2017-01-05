12fddc03f01cba31aa61ea037a94c13ee67846d4
feitzi <mathias@feitzinger.org>
2017-01-05 13:43:35 +0100

    init CI-deployment repo

--- src/main.browser.ts
+++
@@ -1,8 +0,0 @@
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {AppModule} from './app/app.module';
-require('@angular/material/core/theming/prebuilt/indigo-pink.css');
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
- 
